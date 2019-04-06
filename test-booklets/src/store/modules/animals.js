3b43feec12dbad77823e273b9f9b2f743f09b10d
iscorekin <iscorekin@gmail.com>
2019-04-07 00:39:58 +0500

    Simplify animals list module

--- test-booklets/src/store/modules/animals.js
+++ test-booklets/src/store/modules/animals.js
@@ -1,14 +1,15 @@
+import axios from "axios";
 
-const initialState = {
-    animals: [],
+export function fetchAnimals(url) {
+    return dispatch => axios.get(url)
+        .then(response => {dispatch({type: "ANIMALS_FETCH_SUCCESS", payload: response.data})});
 }
 
-const animals = (state = initialState, action) => {
+const animals = (state = {animals: []}, action) => {
     switch(action.type) {
-        case "ANIMALS_REQUEST":
-            return state;
         case "ANIMALS_FETCH_SUCCESS":
-            return action.animals;
+            console.log(action)
+            return {...state, animals: action.payload};
         default:
             return state;
     }
