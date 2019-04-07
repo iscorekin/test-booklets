69447d12567eecb9b681dcdc40aaf4aa655c079d
iscorekin <iscorekin@gmail.com>
2019-04-07 15:24:54 +0500

    Reorder routes

--- test-booklets/src/App.js
+++ test-booklets/src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Route, Switch, Router } from "react-router";
 import { history } from "./store";
 import Animals from "./pages/Animals";
-import Animal from "./pages/Animal";
 import Layout from "./layouts";
+import AnimalPage from "./pages/Animal";
 
 const App = () => (
   <Router history={history}>
     <Layout>
       <Switch>
+        <Route path="/animal/:id" component={AnimalPage} />
         <Route path="/" component={Animals} />
-        <Route path="/animal/:id" component={Animal} />
       </Switch>
     </Layout>
   </Router>
